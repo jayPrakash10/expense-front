240ba628dc4d63d89512e3edf5dbb96cfb52083f
Jay Prakash Kumar <10125prakash@gmail.com>
2025-06-22 18:19:50 +0530

    Github deployment configuration Added

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "" : "",
+  basePath: isProd ? "/expense-front" : "",
+  output: "export"
 };
 
 export default nextConfig;
